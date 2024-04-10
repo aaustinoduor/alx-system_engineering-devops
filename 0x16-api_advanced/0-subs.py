f5d74b4f34e0523600be3d1678a71d64cc50eda3
aaustinoduor <aaustinoduorr@gmail.com>
2024-04-10 04:01:28 +0300

    api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,7 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
+    if response.status_code == 300:
         data = response.json()
         subscribers = data['data']['subscribers']
         return subscribers
